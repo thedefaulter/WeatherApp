d65772a7e0adf5518fcf072fc686c4a9760992f8
Satyam Kumar <56799378+thedefaulter@users.noreply.github.com>
2024-08-01 06:28:19 +0530

    Update api.js
    changed the API

--- api.js
+++ api.js
@@ -5,12 +5,12 @@ async function getData() {
 	const percentage = "%";
 	const m= "m/h";
 
-	const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city;
+	const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + city;
 	const query = {
 		method: 'GET',
 		headers: {
 			'X-RapidAPI-Key': '3460ade1efmsh597ccc5b6ce1e07p14573fjsnefba74dfb616',
-			'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+			'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
 		}
 	}
 	
@@ -21,15 +21,15 @@ async function getData() {
 
 		/*document.getElementById("city").innerHTML = city;*/
 
-		document.getElementById("temp").innerHTML = result.temp;
-        document.getElementById("max_temp").innerHTML = result.max_temp;
-        document.getElementById("min_temp").innerHTML = result.min_temp;
+		document.getElementById("temp").innerHTML = "- " + result.current.temp_c + " °C";
+        document.getElementById("max_temp").innerHTML = "- " + result.forecast.forecastday[0].day.maxtemp_c + " °C";
+        document.getElementById("min_temp").innerHTML = "- " + result.forecast.forecastday[0].day.mintemp_c + " °C";
 
-		document.getElementById("humidity").innerHTML = result.humidity;
-        document.getElementById("feels_like").innerHTML = result.feels_like;
+		document.getElementById("humidity").innerHTML = "- " + result.current.humidity + " %";
+        document.getElementById("feels_like").innerHTML = "- " + result.current.feelslike_c + " °C";
 		
-        document.getElementById("wind_degrees").innerHTML = result.wind_degrees;
-        document.getElementById("wind_speed").innerHTML = result.wind_speed
+        document.getElementById("wind_degrees").innerHTML =  "- " + result.current.wind_degree + " °";
+        document.getElementById("wind_speed").innerHTML =  "- " + result.current.wind_mph + " m/s";
 
 
 	} catch (error) {
